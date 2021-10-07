37b0ddb44250f71439ef0841fae52d244f3b14aa
lily-liu-17 <90275723+lily-liu-17@users.noreply.github.com>
2021-10-06 21:48:09 -0400

    Create code.py
    1

---
+++ code.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+# Created by: Lily Liu
+# Created on: Oct 2021
+# This program is the "Space Aliens" program on the EdgeBadge
+
+import ugame
+import stage
+
+def game_scene():
+    #this function is he main game game_scene
+
+    print("\n\n\n") # 3 blank lines
+    print ("Hello, World!")
+
+    # repeat forever, game loop
+    while True:
+        pass # just a placeholder for now
+
+if __name__ == "__main__":
+    game_scene()
