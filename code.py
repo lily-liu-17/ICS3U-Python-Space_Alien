6ddbe0e9ed6a15d4daf0a77aa7f915d0f5679c4a
lily-liu-17 <90275723+lily-liu-17@users.noreply.github.com>
2021-10-06 22:04:23 -0400

    Update code.py

--- code.py
+++ code.py
@@ -8,13 +8,22 @@ import ugame
 import stage
 
 def game_scene():
-    #this function is he main game game_scene
-
+    # this function is he main game game_scene
+    
+    # image blanks for circut python
     image_bank_background = stage.Bank.from_bmp16("space_aliens_background.bmp")
+    
+    # set the background image to 0 in the image blank
+    # and the size (10 x 8 tiles of size 16 x 16)
     background = stage.Grid(image_bank_background, 10, 8)
     
+    # set the background to image 0 in the image blanks
+    # and set the frame rate to 60fps
     game = stage.Stage(ugame.display,60)
+    # set the layer of all sprites, item show up in order
     game.layers = [background]
+    # render all sprites
+    # most likely you will only render the background once per game
     game.render_block()
     
     # repeat forever, game loop
@@ -23,4 +32,4 @@ def game_scene():
 
 if __name__ == "__main__":
     game_scene()
-   
+    
