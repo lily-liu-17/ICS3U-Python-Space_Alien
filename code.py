f81a8a9eaf0c83b61333587d0c39f3962d70c64d
lily-liu-17 <90275723+lily-liu-17@users.noreply.github.com>
2021-10-06 21:49:01 -0400

    2

--- code.py
+++ code.py
@@ -10,12 +10,17 @@ import stage
 def game_scene():
     #this function is he main game game_scene
 
-    print("\n\n\n") # 3 blank lines
-    print ("Hello, World!")
-
+    image_bank_background = stage.Bank.from_bmp16("space_aliens_background.bmp")
+    background = stage.Grid(image_bank_background, 10, 8)
+    
+    game = stage.Stage(ugame.display,60)
+    game.layers = [background]
+    game.render_block()
+    
     # repeat forever, game loop
     while True:
         pass # just a placeholder for now
 
 if __name__ == "__main__":
     game_scene()
+   
