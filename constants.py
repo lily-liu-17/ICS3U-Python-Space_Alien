2bbc1a04fbb28ddd4eeb7d0ad959cea95a096979
lily-liu-17 <90275723+lily-liu-17@users.noreply.github.com>
2021-10-08 19:11:16 -0400

    Create constants.py

---
+++ constants.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+# Created by: Lily Liu
+# Created on: Oct 2021
+# This constants file is for the space aliens game
+
+# PyBadge screen size is 160x128 and spites are 16x16
+SCREEN_X = 160
+SCREEN_Y = 128
+SCREEN_GRID_X = 10
+SCREEN_GRID_Y = 8
+SPRITE_SIZE = 16
+TOTAL_NUMBER_OF_ALIENS = 5
+FPS = 160
+SPRITE_MOVEMENT_SPEED = 1
