ede079128ea544b228ff921c9c79753df1f4fcec
lily-liu-17 <90275723+lily-liu-17@users.noreply.github.com>
2021-10-12 20:11:54 -0400

    Update constants.py

--- constants.py
+++ constants.py
@@ -11,5 +11,13 @@ SCREEN_GRID_X = 10
 SCREEN_GRID_Y = 8
 SPRITE_SIZE = 16
 TOTAL_NUMBER_OF_ALIENS = 5
-FPS = 160
+FPS = 60
 SPRITE_MOVEMENT_SPEED = 1
+
+# Using for button staticmethod
+button_state = {
+    "button_up": "up",
+    "button_just_pressed": "just pressed",
+    "button_still_pressed": "still pressed",
+    "button_released": "released",
+}
